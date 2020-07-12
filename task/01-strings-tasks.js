095bde680a43774a00b6ebbca6dd6ea4fb8bbda5
arturkhmeliarskiy <artur.khmelyarskiy.00@mail.ru>
2020-07-12 18:12:30 +0300

    Task 1-4

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -54,7 +54,7 @@ function getStringLength(value) {
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
 function getStringFromTemplate(firstName, lastName) {
-    return 'Hello, ${firstName} ${lastName}';
+    return `Hello, ${firstName} ${lastName}!`;
 }
 
 /**
@@ -68,7 +68,7 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
 function extractNameFromTemplate(value) {
-    return value.slice(7,1);
+    return value.slice(7,-1);
 }
 
 
