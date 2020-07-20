2355c422d923ace8254037fcdbb6163e0c0e6579
arturkhmeliarskiy <67556260+arturkhmeliarskiy@users.noreply.github.com>
2020-07-20 12:21:22 +0300

    task 1-4

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   return new Date(value);
+   throw new Error('Not implemented');
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   return new Date(value);
+   throw new Error('Not implemented');
 }
 
 
@@ -56,8 +56,7 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   const year = date.getFullYear();
-	return year % 400 === 0 || year % 100 !== 0 && (year % 4 === 0);
+   throw new Error('Not implemented');
 }
 
 
@@ -77,13 +76,7 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   const day = endDate.getDate() - startDate.getDate();
-	const min = endDate.getMinutes() - startDate.getMinutes();
-	let hour = endDate.getHours() - startDate.getHours();
-	const msec = endDate.getMilliseconds() - startDate.getMilliseconds();
-	const sec = endDate.getSeconds() - startDate.getSeconds();
-	day > 0 ? hour += 24 * day : hour;
-	return (hour < 10 ? '0' + hour : hour) + ':' + (min < 10 ? '0' + min : min) + ':' + (sec < 10 ? '0' + sec : sec) + '.' + (msec < 10 ? '00' + msec : msec < 100 ? '0' + msec : msec);
+   throw new Error('Not implemented');
 }
 
 
@@ -101,12 +94,7 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-   let hour = date.getUTCHours();
-	const min = date.getUTCMinutes();
-	hour > 11 ? hour -= 12 : hour;
-	let angle = Math.abs((hour * 60 + min) * 0.5 - 6 * min);
-	angle > 180 ? angle = 360 - angle : angle;
-	return angle * Math.PI / 180;
+    throw new Error('Not implemented');
 }
 
 
